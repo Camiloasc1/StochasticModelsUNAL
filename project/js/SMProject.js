1567894a645ef135ad12f9c5b226dc97c7197b6c
caasanchezcu <caasanchezcu@unal.edu.co>
2017-11-11 10:48:59 -0500

    AngularJS main source file added

---
+++ project/js/SMProject.js
@@ -0,0 +1,19 @@
+"use strict";
+var app = angular.module('SMProject', ['ngRoute']);
+
+app.controller('Calculator', ['$scope', function ($scope) {
+}]);
+
+app.config(['$locationProvider', '$routeProvider', function ($locationProvider, $routeProvider) {
+    $routeProvider
+        .when('/', {
+            templateUrl: 'views/Calculator.html',
+            controller: 'Calculator'
+        })
+
+        .otherwise({redirectTo: '/'});
+
+    //html5mode causes several issues when the front end is embedded with the web service.
+    $locationProvider.html5Mode(true);
+    // $locationProvider.hashPrefix('!');
+}]);
