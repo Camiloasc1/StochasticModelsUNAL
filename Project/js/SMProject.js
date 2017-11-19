e0b96d813371445604b98eb7ab07a8153f3e7ff6
caasanchezcu <caasanchezcu@unal.edu.co>
2017-11-19 12:28:34 -0500

    Arrival proportion, probabilities and mu calculation

--- Project/js/SMProject.js
+++ Project/js/SMProject.js
@@ -8,13 +8,8 @@ app.controller('Calculator', ['$scope', function ($scope) {
     // The imposed restrictions to comply with
     $scope.restrictions = {maxUsage: 0.95, maxQueueLength: 5, maxQueueTime: 5.0};
     // servers list a.k.a shops in the shopping mall
-    $scope.servers = [{name: "Store 1", proportion: 1, serviceRate: 1.0, selected: false, probability: 0.1}];
+    $scope.servers = [{name: "Store 1", proportion: 1.0, serviceRate: 1.0, selected: false, probability: 0.1}];
     $scope.result = 0.0;
-    $scope.queue = new MMc(30, 12, 5);
-
-    $scope.solve = function () {
-        $scope.result = $scope.queue.simulate($scope.restrictions);
-    };
 
     // $scope.$watch('parameters', function () {
     //     if ($scope.parameters.arrivalTime !== 0.0) {
@@ -22,9 +17,32 @@ app.controller('Calculator', ['$scope', function ($scope) {
     //     }
     // }, true);
 
+    $scope.calcProbability = function () {
+        var total = $scope.servers.reduce(function (total, currentValue) {
+            return total + currentValue.proportion;
+        }, 0.0);
+        $scope.servers.forEach(function (currentValue, index, arr) {
+            arr[index].probability = currentValue.proportion / total;
+        });
+    };
+
+    $scope.calcMu = function () {
+        $scope.calcProbability();
+        var mu = $scope.servers.reduce(function (total, currentValue) {
+            return total + currentValue.probability * currentValue.serviceRate;
+        }, 0.0);
+        mu /= $scope.servers.length;
+        return mu;
+    };
+
+    $scope.solve = function () {
+        var queue = new MMc($scope.parameters.arrivalRate, $scope.calcMu(), 1);
+        $scope.result = queue.simulate($scope.restrictions);
+    };
+
     $scope.addNew = function () {
         var name = "Store " + ($scope.servers.length + 1);
-        $scope.servers.push({name: name, proportion: 1, serviceRate: 1.0, selected: false, probability: 0.1})
+        $scope.servers.push({name: name, proportion: 1.0, serviceRate: 1.0, selected: false, probability: 0.1})
     };
 
     $scope.remove = function () {
