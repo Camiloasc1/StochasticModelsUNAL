e1c00b3366615725b29ae2123a5646f8d249b065
caasanchezcu <caasanchezcu@unal.edu.co>
2017-11-21 16:29:46 -0500

    Adding parameter which indicates how many stores are visited by a client

--- Project/js/SMProject.js
+++ Project/js/SMProject.js
@@ -4,11 +4,11 @@ var app = angular.module('SMProject', ['ngRoute']);
 app.controller('Calculator', ['$scope', function ($scope) {
     $scope.selectAll = false;
     // The known parameters
-    $scope.parameters = {arrivalRate: 1.0};
+    $scope.parameters = {arrivalRate: 1.0, visitedStores: 5.0};
     // The imposed restrictions to comply with
     $scope.restrictions = {maxUsage: 0.95, maxQueueLength: 5, maxQueueTime: 5.0};
     // servers list a.k.a shops in the shopping mall
-    $scope.servers = [{name: "Store 1", proportion: 1.0, serviceRate: 1.0, selected: false, probability: 0.1}];
+    $scope.servers = [{name: "Store 1", proportion: 1.0, serviceRate: 10.0, selected: false, probability: 0.1}];
     $scope.result = 0.0;
 
     // $scope.$watch('parameters', function () {
@@ -32,6 +32,7 @@ app.controller('Calculator', ['$scope', function ($scope) {
             return total + currentValue.probability * currentValue.serviceRate;
         }, 0.0);
         mu /= $scope.servers.length;
+        mu /= $scope.parameters.visitedStores; // Visit multiple stores reduces the overall service rate
         return mu;
     };
 
@@ -42,7 +43,7 @@ app.controller('Calculator', ['$scope', function ($scope) {
 
     $scope.addNew = function () {
         var name = "Store " + ($scope.servers.length + 1);
-        $scope.servers.push({name: name, proportion: 1.0, serviceRate: 1.0, selected: false, probability: 0.1})
+        $scope.servers.push({name: name, proportion: 1.0, serviceRate: 10.0, selected: false, probability: 0.1})
     };
 
     $scope.remove = function () {
